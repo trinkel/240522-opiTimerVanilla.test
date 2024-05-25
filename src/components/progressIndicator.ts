a43a3d26d377eab35ad351ab0110842b26b12e0f
Tim Rinkel <trinkel@dbugg.dev>
2024-05-25 13:38:41 -0500

    add initial typescript for progress inidicator

---
+++ src/components/progressIndicator.ts
@@ -0,0 +1,153 @@
+export default class ProgressIndicator extends HTMLElement {
+	calculatedCircumference: number;
+	constructor() {
+		super();
+
+		// NOTE:  Component attribute `mode`: 0 = countdown 1 = count up
+
+		//! Note: this.* variables come from getters defined below.
+
+		// Calculate the circle radius and the normalized version which is radius minus the stroke
+		const radius = this.viewBox / 2;
+		const normalizedRadius = radius - this.stroke;
+		this.calculatedCircumference = normalizedRadius * 2 * Math.PI;
+
+		//  Set the custom property viewbox value for our CSS to latch on to
+		this.style.setProperty('--progress-indicator-viewbox', `${this.viewBox}px`);
+
+		// Set the default aria role states
+		this.setAttribute('aria-label', this.label);
+		this.setAttribute('role', 'progressbar');
+		this.setAttribute('aria-valuemax', this.valueMax.toString());
+
+		//  Render the component with all the data ready
+		this.innerHTML = `
+			<div class="progress-indicator">
+				<div class="progress-indicator__visual">
+					<div data-progress-count="" class="progress-indicator__count"></div>
+					<svg
+						fill='none'
+						viewBox="0 0 ${this.viewBox} ${this.viewBox}"
+						width="${this.viewBox}"
+						height="${this.viewBox}"
+						focusable="false"
+						class="progress-indicator__circle"
+					>
+						<circle
+						r="${normalizedRadius}"
+						cx="${radius}"
+						cy="${radius}"
+						stroke-width="${this.stroke}"
+						class="progress-indicator__background-circle"
+						/>
+						<circle
+						transform="rotate(-90, ${radius}, ${radius})"
+						r="${normalizedRadius}"
+						cx="${radius}"
+						cy="${radius}"
+						stroke-dasharray="${this.calculatedCircumference} ${this.calculatedCircumference}"
+						stroke-width="${this.stroke}"
+						stroke-linecap="round"
+						class="progress-indicator__progress-circle"
+						data-progress-circle
+						/>
+					</svg>
+					<svg
+						class="progress-indicator__check"
+						focusable="false"
+						viewBox="0 0 20 20"
+						fill="none"
+					>
+						<path
+							d="m8.335 12.643 7.66-7.66 1.179 1.178L8.334 15 3.032 9.697 4.21 8.518l4.125 4.125Z"
+							fill="currentColor"
+						/>
+					</svg>
+				</div>
+			</div>
+		`;
+	}
+
+	setProgress(progress: number) {
+		// Always make sure the progress passed never exceeds the max
+		if (progress > this.valueMax) {
+			progress = this.valueMax;
+		}
+
+		// Always make sure the progress passed is never less than 0
+		if (progress < 0) {
+			progress = 0;
+		}
+
+		//  Set the aria role value for screen readers
+		this.setAttribute('aria-valuenow', progress.toString());
+
+		const circle = this.querySelector('[data-progress-circle]') as HTMLElement;
+		const progressCount = this.querySelector('[data-progress-count]');
+
+		// Calculate a dash offset value based on the calculated circumference and the current percentage
+		circle
+			? (circle.style.strokeDashoffset = (
+					this.calculatedCircumference -
+					(progress / this.valueMax) * this.calculatedCircumference
+			  ).toString())
+			: null;
+
+		// A human readable version for the text label
+		progressCount
+			? (progressCount.textContent = `${progress}${this.unit}`)
+			: null;
+
+		//  Set a complete or pending state base on progress
+		if (progress <= 0) {
+			this.setAttribute('data-progress-state', 'complete');
+		} else {
+			this.setAttribute('data-progress-state', 'pending');
+		}
+		if (this.mode) {
+		} else {
+			if (progress >= this.valueMax) {
+				this.setAttribute('data-progress-state', 'complete');
+			} else {
+				this.setAttribute('data-progress-state', 'pending');
+			}
+		}
+	}
+
+	static get observedAttributes() {
+		return ['progress'];
+	}
+
+	attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+		if (name === 'progress') {
+			this.setProgress(Number(newValue));
+		}
+	}
+
+	get valueMax(): number {
+		return Number(this.getAttribute('value-max')) || 60;
+	}
+
+	get unit(): string {
+		return this.getAttribute('unit') || '';
+	}
+
+	get mode(): number {
+		return Number(this.getAttribute('mode')) || 0; // 1 = Countdown | 0 = Count up
+	}
+
+	get viewBox(): number {
+		return Number(this.getAttribute('viewbox')) || 100;
+	}
+
+	get stroke(): number {
+		return Number(this.getAttribute('stroke')) || 5;
+	}
+
+	get label(): string {
+		return this.getAttribute('label') || 'Current progress';
+	}
+}
+
+customElements.define('progress-indicator', ProgressIndicator);
+//! End of custom element definition
