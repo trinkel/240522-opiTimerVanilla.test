038cff728b13ff9066e91e04c4f401b300ee8f35
Tim Rinkel <trinkel@dbugg.dev>
2024-06-04 07:27:02 -0500

    add sample data for testing and demo

---
+++ src/data/sampleData.ts
@@ -0,0 +1,47 @@
+interface teams {
+	name: string;
+	level: string;
+	startTime: string;
+	endTime: string;
+	duration: number;
+}
+[];
+
+// Placeholder team teams
+const teams = [
+	{
+		name: 'Glenview Blades',
+		level: 'Preliminary',
+		startTime: '12:00 PM',
+		endTime: '12:07 PM',
+		duration: 7,
+	},
+	{
+		name: 'Northernettes',
+		level: 'Preliminary',
+		startTime: '12:07 PM',
+		endTime: '12:14 PM',
+		duration: 7,
+	},
+	{
+		name: 'Saint Louis Synergy',
+		level: 'Preliminary',
+		startTime: '12:14 PM',
+		endTime: '12:21 PM',
+		duration: 7,
+	},
+	{
+		name: 'Crystallettes',
+		level: 'Preliminary',
+		startTime: '12:21 PM',
+		endTime: '12:28 PM',
+		duration: 7,
+	},
+	{
+		name: 'Chicago Skates',
+		level: 'Preliminary',
+		startTime: '12:28 PM',
+		endTime: '12:35 PM',
+		duration: 7,
+	},
+];
