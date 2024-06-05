6bf7a0892de1b8b29a20a94808ebb5f7b9d1839c
Tim Rinkel <trinkel@dbugg.dev>
2024-06-05 06:59:29 -0500

    move old timing code to resource folder

--- src/ts/team.ts
+++ src/ts/team.ts
@@ -30,47 +30,14 @@ function setTime(time: string): Date {
 	return date;
 }
 
-// todo: deprecated by date-fns?
-interface timeMath {
-	days: number;
-	hours: number;
-	minutes: number;
-	seconds: number;
-}
+export class teamSession {
 
-// todo: deprecated by date-fns?
-const timeMath = {
-	days: 1000 * 60 * 60 * 24,
-	hours: 1000 * 60 * 60,
-	minutes: 1000 * 60,
-	seconds: 1000,
-};
+	constructor(teams) {
+		this.teams = teams
+	}
 
-type setup = {
-	name: string;
-	startTime: string;
-	sessionSpec: number;
-	endTime: string;
-}[];
+	this.teams.
 
-// Placeholder team setup
-const setup = [
-	{
-		name: 'Rhythm & Blades',
-		startTime: '23:59',
-		sessionSpec: 8,
-		endTime: '12:08',
-	},
-];
-
-//todo: get time remaining to all times (intervalToDuration as separate function)?
-interface teamSession {
-	name: string;
-	startTime: '';
-}
-const { name, startTime, sessionSpec, endTime } = setup[0];
-
-const teamSession = {
 	// Set team basics from ${setup}
 
 	// add a team name if none provided for demo purposes
