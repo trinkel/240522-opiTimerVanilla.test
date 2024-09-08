24a68c42865d87d541faed74f9984e0108514ad4
Tim Rinkel <trinkel@dbugg.dev>
2024-09-07 19:09:57 -0500

    add objectifySeconds and stringifySeconds time utilities

--- src/ts/timeUtilities.ts
+++ src/ts/timeUtilities.ts
@@ -78,3 +78,54 @@ export const timeUnits: TimeUnits = {
 	 */
 	weeks: 60 * 60 * 24 * 7,
 };
+
+/**
+ * Converts seconds to an object of hours, minutes and seconds
+ *
+ * @export
+ * @param {number} totalSeconds
+ * @returns {{ hours: number; minutes: number; seconds: number; }}
+ */
+export function objectifySeconds(totalSeconds: number) {
+	const hours: number = Math.floor(totalSeconds / timeUnits.hours);
+	totalSeconds %= timeUnits.hours;
+	const minutes: number = Math.floor(totalSeconds / timeUnits.minutes);
+	const seconds: number = totalSeconds % timeUnits.minutes;
+	return { hours, minutes, seconds };
+}
+
+/**
+ * Converts seconds to hour/minute/second string
+ * Caveats: Only one format available. No days.
+ *
+ * @export
+ * @param {number} seconds
+ * @param {string} format
+ * @returns {string}
+ */
+export function stringifySeconds(
+	seconds: number,
+	format: string = 'text'
+): string {
+	let str = '';
+	switch (format) {
+		case '':
+			console.log('empty string');
+			break;
+		default:
+			let result = objectifySeconds(seconds);
+			str += result.hours
+				? `${result.hours} ${result.hours === 1 ? 'hour' : 'hours'}`
+				: '';
+			str += result.hours && (result.minutes || result.seconds) ? `, ` : '';
+			str +=
+				result.minutes || (result.hours && result.seconds)
+					? `${result.minutes} ${result.minutes === 1 ? 'minute' : 'minutes'}`
+					: '';
+			str += (result.hours || result.minutes) && result.seconds ? `, ` : '';
+			str += result.seconds
+				? `${result.seconds} ${result.seconds === 1 ? 'second' : 'seconds'}`
+				: '';
+	}
+	return str;
+}
