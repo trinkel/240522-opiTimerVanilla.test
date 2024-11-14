a0a8d3574cdeec29d1592a50c4fa3a2f0661dfc6
Tim Rinkel <trinkel@dbugg.dev>
2024-11-13 22:23:20 -0600

    fix template literal strings in settings form
    Add quote marks

--- src/components/settingsForm.ts
+++ src/components/settingsForm.ts
@@ -2,8 +2,7 @@ import { appDefaults } from '../data/appDefaults';
 import { stringifySeconds } from '../ts/timeUtilities';
 
 export const settingsForm = `
-		<sl-drawer label="Settings" id="settings" style="--size: max-content">
-			<h1>Settings TS</h1>
+		<sl-drawer label="Settings" id="settings" open style="--size: max-content">
 			<form id="settings-form">
 				<fieldset id="start">
 					<sl-input id="start-time" name="start-time" class="label-on-left"
@@ -14,7 +13,7 @@ export const settingsForm = `
 				<fieldset id="timing">
 					<sl-select id="practice-length" name="practice-length"
 						class="label-on-left" label="Practice Length"
-						help-text="Practice Length from schedule or 104" value=${appDefaults.practiceLength.toString()} clearable
+						help-text="Practice Length from schedule or 104" value="${appDefaults.practiceLength.toString()}" clearable
 						required>
 						<sl-option value="6">6 minutes</sl-option>
 						<sl-option value="7">7 minutes</sl-option>
@@ -26,22 +25,26 @@ export const settingsForm = `
 					<sl-radio-group id="pause-between-selector"
 						name="pause-between-selector" class="label-on-left"
 						label="Pause between teams"
-						help-text="Will there be a pause between teams" value=${
+						help-text="Will there be a pause between teams" value="${
 							appDefaults.pauseBetweenSelector
-						}>
+						}">
 						<sl-radio-button value="no" pill>No</sl-radio-button>
 						<sl-radio-button value="yes" pill>Yes</sl-radio-button>
 					</sl-radio-group>
 					<sl-input id="pause-length" name="pause-length" class="label-on-left"
 						label="Pause Length" disabled help-text="Length of pause in minutes:seconds"
 						placeholder="00:00"
-						 value=${stringifySeconds(appDefaults.pauseLength, false, 'colon')}></sl-input>
+						 value="${stringifySeconds(
+								appDefaults.pauseLength,
+								false,
+								'colon'
+							)}"></sl-input>
 				</fieldset>
 
 				<fieldset id="operation-mode">
 					<sl-radio-group id="operation-mode-selector"
 						name="operation-mode-selector" class="label-on-left"
-						label="Select operation mode" value=${appDefaults.operationMode} required>
+						label="Select operation mode" value="${appDefaults.operationMode}" required>
 						<sl-radio-button value="anonymous" pill>
 							Anonymous
 						</sl-radio-button>
@@ -52,9 +55,9 @@ export const settingsForm = `
 					<sl-input id="number-teams" name="number-teams"
 						class="label-on-left" label="Number of teams"
 						help-text="Enter the number of teams in this group" type="number"
-						min="1" step="1" value=${appDefaults.numberTeams.toString()} required></sl-input>
+						min="1" step="1" value="${appDefaults.numberTeams.toString()}" required></sl-input>
 					<sl-textarea id="team-list" name="team-list"
-						class="label-on-left" label="Team List" value=${appDefaults.teamList} disabled
+						class="label-on-left" label="Team List" value="${appDefaults.teamList}" disabled
 						help-text="Names of the teams in this group. One team per line (copy/paste is our friend)"
 						resize="auto" required></sl-textarea>
 				</fieldset>
