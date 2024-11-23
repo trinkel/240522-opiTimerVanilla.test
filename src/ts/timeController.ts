72e609386565d783df4ffd2492f67bb2af8850a3
Tim Rinkel <trinkel@dbugg.dev>
2024-11-23 17:15:51 -0600

    [refactor] Change formatting of timer display text from homegrown to `date-fns.format`
    **timeController.ts**
    - Change `formatTime` to `date-fns.format`

--- src/ts/timeController.ts
+++ src/ts/timeController.ts
@@ -2,6 +2,7 @@
 import {
 	addMinutes,
 	addSeconds,
+	format,
 	setHours,
 	setMinutes,
 	setSeconds,
@@ -179,31 +180,12 @@ export class TimeController {
 		}
 
 		this.timeRemaining.progress = interval;
-		this.timeRemaining.display = this.formatTime(interval);
+		this.timeRemaining.display =
+			interval < timeUnits.hours * timeUnits.millies // 1 hour
+				? format(interval, 'm:ss')
+				: format(interval, 'h:mm:ss');
 
 		return this.timeRemaining;
-	}
-
-	/**
-	 * formatTime: converts timestamp to hours: seconds:minutes time string format 00:00:00 (hours optional)
-	 *
-	 * @param {number} timeStamp
-	 * @returns {string}
-	 */
-	formatTime(timeStamp: number): string {
-		let totalSeconds = Math.round(timeStamp / timeUnits.millies);
-		const hours = Math.floor(totalSeconds / timeUnits.hours);
-		totalSeconds %= timeUnits.hours;
-		const minutes = Math.floor(totalSeconds / timeUnits.minutes);
-		const seconds = totalSeconds % timeUnits.minutes;
-
-		if (hours) {
-			return `${hours.toString()}:${minutes
-				.toString()
-				.padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-		} else {
-			return `${minutes.toString()}:${seconds.toString().padStart(2, '0')}`;
-		}
 	}
 
 	/**
