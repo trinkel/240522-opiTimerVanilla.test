2c5febcca5901f61d8bf80b82f885f8540164a1a
Tim Rinkel <trinkel@dbugg.dev>
2024-11-28 09:12:18 -0600

    [style] Tighten up settings form spacing
    **_settings.css**
    - Change form input widths from `min-content` to `max-content`
    
    **settingsForm.ts**
    - Remove `help-text` in favor of `<sl-tooltips>`
    - Remove `clearable` from `practice-length` select list
    - Adjust indents for `sl-tooltip` wraps

--- src/components/settingsForm.ts
+++ src/components/settingsForm.ts
@@ -39,10 +39,7 @@ export class SettingsForm {
 				<fieldset id="start">
 					<sl-radio-group id="start-type"
 						name="start-type" class="label-on-left"
-						label="Group start mode"
-						help-text="Start group at scheduled time or manually by button" value="${
-							this.groupStartType
-						}">
+						label="Group start mode" value="${this.groupStartType}">
 						<sl-tooltip content="Start at designated Start Time">
 							<sl-radio-button value="scheduled" pill>Scheduled</sl-radio-button>
 						</sl-tooltip>
@@ -51,62 +48,63 @@ export class SettingsForm {
 						</sl-tooltip>
 					</sl-radio-group>
 
-					<sl-input id="start-time" name="start-time" class="label-on-left"
-						label="Start Time" help-text="Start-time for this session (eg: 10:45 AM)" type="time" value="${
-							this.groupStartTimeStr
-						}" autofocus
-						required></sl-input>
-					<sl-select id="practice-length" name="practice-length"
-						class="label-on-left" label="Practice Length"
-						help-text="Practice Length from schedule or 104" value="${this.practiceLength.toString()}" clearable
-						required>
-						${this.demo ? '<sl-option value="2">2 minutes</sl-option>' : ''}
-						<sl-option value="6">6 minutes</sl-option>
-						<sl-option value="7">7 minutes</sl-option>
-						<sl-option value="8">8 minutes</sl-option>
-						<sl-option value="10">10 minutes</sl-option>
-						<sl-option value="11">11 minutes</sl-option>
-						<sl-option value="12">12 minutes</sl-option>
-					</sl-select>
+					<sl-tooltip content='"eg: 10:45 AM"' distance=-20>
+						<sl-input id="start-time" name="start-time" class="label-on-left"
+							label="Start Time" type="time" value="${this.groupStartTimeStr}" autofocus
+							required></sl-input>
+					</sl-tooltip>
+					<sl-tooltip content="See schedule or 104" distance=-20>
+						<sl-select id="practice-length" name="practice-length"
+							class="label-on-left" label="Practice Length"
+							value="${this.practiceLength.toString()}"
+							required>
+							${this.demo ? '<sl-option value="2">2 minutes</sl-option>' : ''}
+							<sl-option value="6">6 minutes</sl-option>
+							<sl-option value="7">7 minutes</sl-option>
+							<sl-option value="8">8 minutes</sl-option>
+							<sl-option value="10">10 minutes</sl-option>
+							<sl-option value="11">11 minutes</sl-option>
+							<sl-option value="12">12 minutes</sl-option>
+						</sl-select>
+					</sl-tooltip>
 				</fieldset>
 
 				<fieldset id="timing">
 					<sl-radio-group id="pause-between-selector"
 						name="pause-between-selector" class="label-on-left"
 						label="Pause between teams"
-						help-text="Will there be a pause between teams" value="${
-							this.pauseBetweenSelector
-						}">
+						value="${this.pauseBetweenSelector}">
 						<sl-radio-button value="no" pill>No</sl-radio-button>
 						<sl-radio-button value="yes" pill>Yes</sl-radio-button>
 					</sl-radio-group>
-					<sl-input id="pause-length" name="pause-length" class="label-on-left"
-						label="Pause Length" disabled help-text="Length of pause in minutes:seconds"
-						placeholder="00:00"
-						 value="${stringifySeconds(this.pauseLength, false, 'colon')}"></sl-input>
+					<sl-tooltip content="<minutes>:<seconds>" distance=-20>
+						<sl-input id="pause-length" name="pause-length" class="label-on-left"
+							label="Pause Length" disabled 						placeholder="00:00"
+							value="${stringifySeconds(this.pauseLength, false, 'colon')}"></sl-input>
+					</sl-tooltip>
 				</fieldset>
 
 				<fieldset id="operation-mode">
 					<sl-radio-group id="operation-mode-selector"
 					name="operation-mode-selector" class="label-on-left"
-					label="Select operation mode" help-text="Session with or without team names" value="${
-						this.operationMode
-					}" required>
+					label="Select operation mode" value="${this.operationMode}" required>
 						<sl-tooltip content="Group based on Number of teams only">
-							<sl-radio-button id="anonymous-mode" name="anonymous-mode" value="anonymous" pill>
-								Anonymous
-							</sl-radio-button>
+								<sl-radio-button id="anonymous-mode" name="anonymous-mode" value="anonymous" pill>
+									Anonymous
+								</sl-radio-button>
 						</sl-tooltip>
-						<sl-tooltip content="Supply list of teams">
-							<sl-radio-button id="team-list-mode" name="team-list-mode" value="list" pill>
-								Team List
-							</sl-radio-button>
+						<sl-tooltip content="Supply list of teams below">
+								<sl-radio-button id="team-list-mode" name="team-list-mode" value="list" pill>
+									Team List
+								</sl-radio-button>
 						</sl-tooltip>
 					</sl-radio-group>
-					<sl-input id="number-teams" name="number-teams"
-						class="label-on-left" label="Number of teams"
-						help-text="Enter the number of teams in this group" type="number"
-						min="1" step="1" value="${this.numberTeams.toString()}" required></sl-input>
+					<sl-tooltip content="Number of teams in this group" distance=-20>
+						<sl-input id="number-teams" name="number-teams"
+							class="label-on-left" label="Number of teams"
+							type="number"
+							min="1" step="1" value="${this.numberTeams.toString()}" required></sl-input>
+					</sl-tooltip>
 					<sl-textarea id="team-list" name="team-list"
 						class="label-on-left" label="Team List" value="${this.teamList}" disabled
 						help-text="Names of the teams in this group. One team per line (copy/paste is our friend)"
