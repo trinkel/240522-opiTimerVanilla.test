trinkel/240522-opiTimerVanilla.test

commit 1edf495ce11aa7e88dfd8114d4ef18fc2617247b

[refactor] Move startTimer from `componentController.ts` to `main.ts`
**main.ts**
- add `startTimer` function
- assign return value to `lastLoop`

**componentController.ts**
- remove `startTimer` function
- return `this.before` and `progressComplete` from `timer()`

src/ts/componentController.ts
@@ -200,7 +200,7 @@ export class ComponentController {
 		});
 	}
 
-	timer(timeController: TimeController): void {
+	timer(timeController: TimeController, progressComplete: boolean): Date {
 		/**
 		 * @Description Get the current time. If `warp` is defined, manipulate time for debug or demo purposes.
 		 * @parameters for `warpJump`: current time and previous time.
@@ -212,7 +212,7 @@ export class ComponentController {
 				: timeController.warpJump(this.before);
 
 		// initialize progress complete test for each loop
-		this.progressComplete = true;
+		progressComplete = true;
 
 		// This may be unused
 		this.sessionStatus
@@ -319,29 +319,20 @@ export class ComponentController {
 					(indicator.element.getAttribute('data-progress-state') as string) !==
 					'complete'
 				) {
-					this.progressComplete = false;
+					progressComplete = false;
 				}
 			});
 		}
 
 		// Hold value of current time for next loop
 		this.before = now;
-		// Reference value
+		// Reference value (Not used)
 		this.iterator++;
+
+		return this.before;
 	}
 
-	startTimer(timeController: TimeController) {
-		return new Promise<void>((resolve, reject) => {
-			const startTimeIntvId = setInterval(() => {
-				this.timer(timeController);
-				// Here 241004: Something to set state of active indicator goes here. Switch that falls through to set a data-state attribute?
-				if (this.progressComplete) {
-					clearInterval(startTimeIntvId);
-					resolve();
-				}
-			}, timeController.tick);
-		});
-	}
+	//! startTimer() Moved to main.ts
 
 	complete(): void {
 		console.log(`[COMPLETE] ---Process Complete: ${this.numberTeams}`);

src/ts/main.ts
@@ -159,7 +159,7 @@ async function startPracticeGroup(timeController: TimeController) {
 			clockBadges.setClocksStartTime(timeController.teamStartTime);
 			clockBadges.setClocksEndTime(timeController.endSession);
 			// Run new team session
-			await componentController.startTimer(timeController);
+			await startTimer(timeController);
 		}
 	} catch (error) {
 		console.log((error as Error).message);
@@ -167,6 +167,24 @@ async function startPracticeGroup(timeController: TimeController) {
 
 	//! This one will be held back
 	console.log(`---End of Async function---`);
+}
+
+function startTimer(timeController: TimeController) {
+	return new Promise<void>((resolve, reject) => {
+		const startTimeIntvId = setInterval(() => {
+				lastLoop = componentController.timer(
+					timeController,
+					parameters.progressComplete //! Not needed (when did we add)?
+				);
+			}
+
+			// Here 241004: Something to set state of active indicator goes here. Switch that falls through to set a data-state attribute?
+			if (parameters.progressComplete) {
+				clearInterval(startTimeIntvId);
+				resolve();
+			}
+		}, timeController.tick);
+	});
 }
 
 //! Call waitTimer

commit 8dfae4271b533538bf8404ed96e5e714002bad11

[fix] Fix `startTimer()` not continuing after firs loop (broken in 1edf495)
`progressComplete` was not being passed back to `startTimer()` to break `setInterval()` to continue to next loop.

**componentController.ts**
- Change return for `timer()` to object `passBack`
- Add `progressComplete` to `passBack`
- Add correct return types for `timer()`

**main.ts**
- Change assignment of startTimer() to object `passBack` which contains properties `lastLoop` and `progressComplete`
- Extract `passBack` properties to variables

src/ts/componentController.ts
@@ -200,7 +200,13 @@ export class ComponentController {
 		});
 	}
 
-	timer(timeController: TimeController, progressComplete: boolean): Date {
+	timer(
+		timeController: TimeController,
+		progressComplete: boolean
+	): {
+		lastLoop: Date;
+		progressComplete: boolean;
+	} {
 		/**
 		 * @Description Get the current time. If `warp` is defined, manipulate time for debug or demo purposes.
 		 * @parameters for `warpJump`: current time and previous time.
@@ -329,7 +335,12 @@ export class ComponentController {
 		// Reference value (Not used)
 		this.iterator++;
 
-		return this.before;
+		const passBack = {
+			lastLoop: this.before,
+			progressComplete: progressComplete,
+		};
+
+		return passBack;
 	}
 
 	//! startTimer() Moved to main.ts

src/ts/main.ts
@@ -182,10 +182,12 @@ function startTimer(timeController: TimeController) {
 					lastLoop = pauseTimer(timeController, lastLoop);
 				}
 			} else {
-				lastLoop = componentController.timer(
+				const passBack = componentController.timer(
 					timeController,
-					parameters.progressComplete //! Not needed (when did we add)?
+					parameters.progressComplete
 				);
+				lastLoop = passBack.lastLoop;
+				parameters.progressComplete = passBack.progressComplete;
 			}
 
 			// Here 241004: Something to set state of active indicator goes here. Switch that falls through to set a data-state attribute?